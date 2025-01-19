edb4716b7df86b10c6e319ddb301dc61fc293984
unknown <nishant.guvvada@gmail.com>
2025-01-19 14:17:23 +0530

    updated eslint - switched off rules

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -9,6 +9,14 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals", "next/typescript")];
+const eslintConfig = [
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript"],
+    rules: {
+      'next/core-web-vitals': 'off',
+      'next/typescript': 'off',
+    },
+  }),
+]
 
 export default eslintConfig;
